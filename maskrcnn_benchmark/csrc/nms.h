3d77ccd8f4de7f2f32f4383a3ff0a2fa3240370e
maxsieg <maxsieg@gmail.com>
2018-11-12 14:44:07 -0800

    Trying to change the c files

--- maskrcnn_benchmark/csrc/nms.h
+++ maskrcnn_benchmark/csrc/nms.h
@@ -1,10 +1,7 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #pragma once
 #include "cpu/vision.h"
-
-#ifdef WITH_CUDA
 #include "cuda/vision.h"
-#endif
 
 
 at::Tensor nms(const at::Tensor& dets,
@@ -12,15 +9,11 @@ at::Tensor nms(const at::Tensor& dets,
                const float threshold) {
 
   if (dets.type().is_cuda()) {
-#ifdef WITH_CUDA
     // TODO raise error if not compiled with CUDA
     if (dets.numel() == 0)
       return at::empty({0}, dets.options().dtype(at::kLong).device(at::kCPU));
     auto b = at::cat({dets, scores.unsqueeze(1)}, 1);
     return nms_cuda(b, threshold);
-#else
-    AT_ERROR("Not compiled with GPU support");
-#endif
   }
 
   at::Tensor result = nms_cpu(dets, scores, threshold);
