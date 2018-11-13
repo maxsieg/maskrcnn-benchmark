e632e902856a85567a8e8f1eedcb60bcbd8955eb
maxsieg <maxsieg@gmail.com>
2018-11-12 17:20:06 -0800

    -More changes to c code to make on the fly compilation work. -Removed debugging.

--- maskrcnn_benchmark/csrc/ROIAlign.h
+++ maskrcnn_benchmark/csrc/ROIAlign.h
@@ -3,9 +3,7 @@
 
 #include "cpu/vision.h"
 
-#ifdef WITH_CUDA
 #include "cuda/vision.h"
-#endif
 
 // Interface for Python
 at::Tensor ROIAlign_forward(const at::Tensor& input,
@@ -15,11 +13,7 @@ at::Tensor ROIAlign_forward(const at::Tensor& input,
                             const int pooled_width,
                             const int sampling_ratio) {
   if (input.type().is_cuda()) {
-#ifdef WITH_CUDA
     return ROIAlign_forward_cuda(input, rois, spatial_scale, pooled_height, pooled_width, sampling_ratio);
-#else
-    AT_ERROR("Not compiled with GPU support");
-#endif
   }
   return ROIAlign_forward_cpu(input, rois, spatial_scale, pooled_height, pooled_width, sampling_ratio);
 }
@@ -35,11 +29,7 @@ at::Tensor ROIAlign_backward(const at::Tensor& grad,
                              const int width,
                              const int sampling_ratio) {
   if (grad.type().is_cuda()) {
-#ifdef WITH_CUDA
     return ROIAlign_backward_cuda(grad, rois, spatial_scale, pooled_height, pooled_width, batch_size, channels, height, width, sampling_ratio);
-#else
-    AT_ERROR("Not compiled with GPU support");
-#endif
   }
   AT_ERROR("Not implemented on the CPU");
 }
--- maskrcnn_benchmark/csrc/ROIPool.h
+++ maskrcnn_benchmark/csrc/ROIPool.h
@@ -3,9 +3,7 @@
 
 #include "cpu/vision.h"
 
-#ifdef WITH_CUDA
 #include "cuda/vision.h"
-#endif
 
 
 std::tuple<at::Tensor, at::Tensor> ROIPool_forward(const at::Tensor& input,
@@ -14,11 +12,7 @@ std::tuple<at::Tensor, at::Tensor> ROIPool_forward(const at::Tensor& input,
                                 const int pooled_height,
                                 const int pooled_width) {
   if (input.type().is_cuda()) {
-#ifdef WITH_CUDA
     return ROIPool_forward_cuda(input, rois, spatial_scale, pooled_height, pooled_width);
-#else
-    AT_ERROR("Not compiled with GPU support");
-#endif
   }
   AT_ERROR("Not implemented on the CPU");
 }
@@ -35,11 +29,7 @@ at::Tensor ROIPool_backward(const at::Tensor& grad,
                                  const int height,
                                  const int width) {
   if (grad.type().is_cuda()) {
-#ifdef WITH_CUDA
     return ROIPool_backward_cuda(grad, input, rois, argmax, spatial_scale, pooled_height, pooled_width, batch_size, channels, height, width);
-#else
-    AT_ERROR("Not compiled with GPU support");
-#endif
   }
   AT_ERROR("Not implemented on the CPU");
 }
